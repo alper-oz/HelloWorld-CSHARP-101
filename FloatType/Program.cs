b5667ae783e333fe493065bd3ebbb17ee5019f03
Alper OZ <alperoz994@gmail.com>
2023-06-17 18:10:00 +0300

    Float veri tipi güncellendi

--- FloatType/Program.cs
+++ FloatType/Program.cs
@@ -10,6 +10,7 @@ namespace FloatType
     {
         static void Main(string[] args)
         {
+            //float veri tipi ondalıklı sayılar tutmamıza olanak tanır
             float ondalikli_sayi = 35.56f; // Burada veri tipi ondalıklı olmasına rağmen sonuna f anahtar kelimesi de ekledik. Bunun sebebi; girdiğiniz değer, ilk double olarak derlenir siz float olduğunu belirtene kadar. 
             Console.WriteLine(ondalikli_sayi);
             Console.ReadLine();
