025f9553c89d8e74ba0c4bd2e56e4056185612d5
Alper OZ <alperoz994@gmail.com>
2023-06-17 23:08:47 +0300

    mini ATM app added.

---
+++ IfElseApply/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IfElseApply
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // KULLANICIDAN 3 NOT GİRDİSİ ALAN VE NOTLARIN ORTALAMASI BULAN VE ORTALAMAYA GÖRE NOTLAMA SİSTEMİ YAPAN UYGULAMA
+            // Console.ReadLine() kullanımına değineceğiz demiştim, burada kullandık
+            
+            Console.WriteLine("Lütfen notunuzu girin ve enter'a basın:"); 
+            int first = Convert.ToInt32(Console.ReadLine()); // kullanıcıdan string formatta aldığımız değeri Convert sınıfının ToInt methodu ile int formata dönüştürüyoruz. Aynı zamanda kullanıcıdan değer almak için Console.ReadLine() kullanıyoruz ve first isimli değişkene atıyoruz. Aynı işlemi second ve third değişkenlerine de yaptık.
+            int second = Convert.ToInt32(Console.ReadLine());
+            int third = Convert.ToInt32(Console.ReadLine());
+            // kullanıcıdan aldığımız 3 notu toplayıp 3'e bölen matematik işlemini de yapıp int türünde result isimle değişkene atıyoruzi
+            int result=(first + second + third)/3;
+
+            //2 farklı değer şartı koymak istersek burdaki 80 ile 100 arasında gibi.. bunun için VE anlamına gelen && sembolünü kullanıyoruz.
+            // bu operatörün amacı 2 DEĞERİ DE TRUE olan şartlar için geçerli. Eğer sonuç 80 den büyük 100'den küçükse bu şartı doğru kabul eder diğer if else bloklarına ilerlemez
+            if (result > 80 && result < 100)
+            {
+                Console.WriteLine("Not değeriniz: A+");
+            }
+            else if (result > 60 && result < 80)
+            {
+                Console.WriteLine("Not değeriniz: A");
+            }
+            else if (result > 40 & result < 60)
+            {
+                Console.WriteLine("Not değeriniz: B+");
+            }
+            else
+            {
+                Console.WriteLine("Not değeriniz: F");
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ IfElseMiniATM/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IfElseMiniATM
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Kullanıcıya yapmak istediği işlemle ilgili soracağız
+            //1- Bakiye Görüntüleme 
+            //2- Para Çekme
+            //3- Para Yatırma
+            // q ile çıkış yapabilirsiniz
+            int amount = 1000;
+            Console.WriteLine("QNB ATM'sine Hoş geldiniz.");
+            Console.WriteLine("Yapmak istediğiniz işlemi tuşlayın");
+            string choose=Console.ReadLine();
+            if(choose == "1")
+            {
+                Console.WriteLine("Şu anki bakiyeniz:" + amount);
+                Console.ReadLine();
+            }
+            else if(choose == "2")
+            {
+                Console.WriteLine("Çekmek istediğiniz tutarı girin:");
+                int withdraw=Convert.ToInt32(Console.ReadLine());
+                if(withdraw<=amount)
+                {
+                    Console.WriteLine("Kalan tutar:" + (amount - withdraw));
+                    Console.ReadLine();
+                }
+                else
+                {
+                    Console.WriteLine("Bakiyeden fazla çekim yapamazsınız.");
+                    Console.ReadLine();
+                }
+                
+            }
+            else if (choose == "3")
+            {
+                Console.WriteLine("Yatırmak istediğiniz tutarı girin:");
+                int deposit=Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Yeni baiyeniz:"+ (amount+deposit));
+                Console.ReadLine();
+            }
+            else if (choose == "q")
+            {
+                Console.WriteLine("Oturumunuz güvenli bir şekilde sonlandırıldı.");
+                Console.ReadLine();
+            }
+            else
+            {
+                Console.WriteLine("Lütfen geçerli bir değer girin:");
+                Console.ReadLine();
+            }
+        }
+    }
+}
