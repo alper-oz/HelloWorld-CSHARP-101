38519f034c31f8f406bcfd2d5b3948c22f5fbf9c
Alper OZ <alperoz994@gmail.com>
2023-06-21 01:21:47 +0300

    Example1/3 comment line update.

--- Example1/Program.cs
+++ Example1/Program.cs
@@ -10,9 +10,9 @@ namespace Example1
     {
         static void Main(string[] args)
         {
-            Console.Write("Bir sayı girin:");
-            int sayi = int.Parse(Console.ReadLine());
-            int karedeger = Karesi(sayi);
+            Console.Write("Bir sayı girin:"); //Kullanıcıdan bir sayı girmesini istedik
+            int sayi = int.Parse(Console.ReadLine()); //girilen değeri string veri tipinden int tipine dönüştürüp sayi isimli değişkene atadık
+            int karedeger = Karesi(sayi); //Girilen sayının karesinin kontrolünü yapabilmek için karedeger adında int veri tipinde değişken atadık, değişkenin içine kullanıcıdan gelen sayi isimli değeri verdik
             if (karedeger > 25)
             {
                 Console.WriteLine("Karesi 25 den büyüktür");
@@ -24,7 +24,7 @@ namespace Example1
             }
             Console.ReadLine();
         }
-        static int Karesi(int a)
+        static int Karesi(int a) //sayının karesini almak için int tipinde statik method yazdık
         {
             return a * a;
         }
